c662be2f19576acfb20a7882e631928ed69d53e6
huhubun <nongzc@outlook.com>
2020-08-13 02:17:56 +0800

    实现输出 IP 和 header 的功能

--- src/BunIp.Web/Pages/Index.cshtml.cs
+++ src/BunIp.Web/Pages/Index.cshtml.cs
@@ -1,10 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
 
 namespace BunIp.Web.Pages
 {
@@ -19,7 +18,30 @@ namespace BunIp.Web.Pages
 
         public void OnGet()
         {
+        }
 
+        public string HeaderRealIp => HttpContext.Request.Headers["X-Real-IP"];
+
+        public IEnumerable<string> HeaderForwardedFor => HttpContext.Request.Headers["X-Forwarded-For"];
+
+        public IPAddress RemoteIpAddress => HttpContext.Connection.RemoteIpAddress;
+
+        public string DisplayIp
+        {
+            get
+            {
+                if (HeaderRealIp != null)
+                {
+                    return HeaderRealIp;
+                }
+
+                if (HeaderForwardedFor != null && HeaderForwardedFor.Any())
+                {
+                    return HeaderForwardedFor.First();
+                }
+
+                return RemoteIpAddress.ToString();
+            }
         }
     }
 }
--- src/BunIp.Web/Startup.cs
+++ src/BunIp.Web/Startup.cs
@@ -41,8 +41,6 @@ namespace BunIp.Web
 
             app.UseRouting();
 
-            app.UseAuthorization();
-
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapRazorPages();
