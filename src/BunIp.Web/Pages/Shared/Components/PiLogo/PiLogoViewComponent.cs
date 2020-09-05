a9b0b9f76d69d165e0cead7ab4e84822e64ff3a3
huhubun <nongzc@outlook.com>
2020-09-06 03:22:59 +0800

    彩蛋：如果部署在树莓派上，会在页面右上角显示树莓派 logo 和树莓派 model 号

---
+++ src/BunIp.Web/Pages/Shared/Components/PiLogo/PiLogoViewComponent.cs
@@ -0,0 +1,28 @@
+﻿using BunIp.Web.Configs;
+using BunIp.Web.Helpers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BunIp.Web.Pages.Shared.Components.PiLogo
+{
+    public class PiLogoViewComponent : ViewComponent
+    {
+        private readonly BunIpConfig _bunIpConfig;
+
+        public PiLogoViewComponent(BunIpConfig bunIpConfig)
+        {
+            _bunIpConfig = bunIpConfig;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+
+            var model = new ViewModel
+            {
+                DeployMode = _bunIpConfig.DeployMode,
+                Model = PiHelper.GetModel(),
+            };
+
+            return View(model);
+        }
+    }
+}
---
+++ src/BunIp.Web/Pages/Shared/Components/PiLogo/ViewModel.cs
@@ -0,0 +1,11 @@
+﻿using BunIp.Web.Configs.BunIpConfigs;
+
+namespace BunIp.Web.Pages.Shared.Components.PiLogo
+{
+    public class ViewModel
+    {
+        public string Model { get; set; }
+
+        public DeployMode DeployMode { get; set; }
+    }
+}
